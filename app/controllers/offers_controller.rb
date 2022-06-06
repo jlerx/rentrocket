099d048d14a25e09834d61e78d8ec6a104fa44b9
Julien Leroux <julien.lerx@gmail.com>
2022-06-06 16:44:44 +0200

    Add actions to offers_controller.rb

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,42 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.All
+  end
+
+  def show
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.save
+    redirect_to offer_path(@offer)
+  end
+
+  def edit
+  end
+
+  def update
+    @offer.update(offer_params)
+    redirect_to offers_path
+  end
+
+  def destroy
+    @offer.destroy
+    redirect_to offers_path
+  end
+
+  private
+
+  def find_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params.require(:offer).permit(:title, :price, :brand, :model, :year, :horsepower, :km_age, :body_type)
+  end
+
 end
