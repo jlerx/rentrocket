1c7b3df3303cad19d13e6f93867e0a5ec8936d57
Valentin Marcoux <valentinmarcoux307@gmail.com>
2022-06-07 15:25:20 +0200

    Fix seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_131144) do
+ActiveRecord::Schema.define(version: 2022_06_07_132234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_131144) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "new"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
@@ -52,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_131144) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin"
     t.string "name"
+    t.string "new"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
