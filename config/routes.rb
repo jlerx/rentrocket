654045b0d3a4444ea4e4b9d8e06a4578197d8480
Julien Leroux <julien.lerx@gmail.com>
2022-06-07 10:41:31 +0200

    Add index to nested route & actions orders_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :offers do
-    resources :orders, only: [:new, :create, :show, :destroy ]
+    resources :orders, only: [:index, :new, :create, :show, :destroy ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
