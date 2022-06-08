dd9e965fc8af3c048ebe29b7bfcfef20f6f6318a
Julien Leroux <julien.lerx@gmail.com>
2022-06-08 15:33:51 +0200

    remove button all offers on homepage + when login succesful redirect to offers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def after_sign_in_path_for(resource)
+    offers_path
+  end
 end
