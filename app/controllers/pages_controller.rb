c072621f2be412835e042edd1faeceb8c0182879
Valentin Marcoux <valentinmarcoux307@gmail.com>
2022-06-09 22:44:54 +0200

    Fix de la HomePage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,10 @@ class PagesController < ApplicationController
     @user = current_user
     @offers = Offer.where(user: current_user)
     @orders = Order.where(user: current_user)
-    #@offer = Offer.new
-
     @demands_reservation = []
     @demands = Order.all
-    @demands.each do |demand|
-      @demands_reservation << demand if demand.offer.user == @user
+    @demands.select do |demand|
+      demand.offer.user == @user
     end
-
   end
 end
