045bf11cd3f44a452209bdf2fc53d5a8275ed8aa
Valentin Marcoux <valentinmarcoux307@gmail.com>
2022-06-09 11:55:03 +0200

    Search bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "controllers"
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "controllers"
-import "bootstrap"
